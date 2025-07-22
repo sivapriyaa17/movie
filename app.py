b0f429b316e3538135437a54e1dfe597b8752cbf
sivapriyaa <sivapriyaa2006@gmail.com>
2025-07-22 22:35:49 +0530

    Moved data files to project and updated paths

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from sklearn.metrics.pairwise import cosine_similarity
 @st.cache_data
 def load_data():
     ratings = pd.read_csv(
-        r"C:\Users\Haripriya\Downloads\ml-100k\ml-100k\u.data",
+        r"data\u.data",
         sep='\t',
         names=['user_id', 'movie_id', 'rating', 'timestamp']
     )
 
     movies = pd.read_csv(
-        r"C:\Users\Haripriya\Downloads\ml-100k\ml-100k\u.item",
+        r"data\u.item",
         sep='|',
         encoding='latin-1',
         names=['movie_id', 'title', 'release_date', 'video_release_date', 'IMDb_URL'] + [f'genre_{i}' for i in range(19)]
